b3cfdd8abf29356e056149183c3afd861b01fd62
Matthew Whitmore <mattwhitmoregit@hotmail.com>
2018-02-14 08:48:12 +0000

    1.5.5
    added rows table. Incomplete

--- FNOLtool/src/test/java/fnolInterfaceCode/GuiCode.java
+++ FNOLtool/src/test/java/fnolInterfaceCode/GuiCode.java
@@ -1,9 +1,15 @@
 package fnolInterfaceCode;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Desktop;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,17 +21,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
 import org.apache.commons.io.FileUtils;
+
+
 import fnolMethods.CollectFnolData;
 import logging.Logger;
 
@@ -40,6 +58,8 @@ public class GuiCode
 	
 	static String deviceReportSaved = "";
 	static String defaultDeviceReportLocation = "";
+	
+	static boolean isSelectedAlready = false;
 	
 	public static void createGui()
 	{
@@ -67,7 +87,7 @@ public class GuiCode
 			
 			
 			String path = "M:\\Cloud Vault\\ID2\\D943E0A6-C9D3-4573-BDD0-DE54DBA3ED92\\0\\14000-14999\\14807\\L\\L\\Claim Flat File (ID 14807).xlsx";
-			String claimsFileCopyPath = f+"\\appFiles\\claimsFileCopy\\flatFile.xlsx";
+			final String claimsFileCopyPath = f+"\\appFiles\\claimsFileCopy\\flatFile.xlsx";
 			Desktop d = null;
 			File mfiles = new File(openMfiles);
 			if(Desktop.isDesktopSupported())
@@ -76,13 +96,12 @@ public class GuiCode
 				{
 					d = Desktop.getDesktop();
 					d.open(mfiles);
+					File file = new File(path);
+					final File copyFile = new File(claimsFileCopyPath);
+					Files.deleteIfExists(copyFile.toPath());
+					FileUtils.copyFile(file, copyFile);
 				}
 			}
-			File file = new File(path);
-			final File copyFile = new File(claimsFileCopyPath);
-			Files.deleteIfExists(copyFile.toPath());
-			FileUtils.copyFile(file, copyFile);
-
 				
 			final int PROGRESS_MIN = 0;
 			final int PROGRESS_MAX = 6500;
@@ -205,7 +224,14 @@ public class GuiCode
 			customiseRows.setEnabled(enableRowsButton(comboOption));
 			startButton.setEnabled(enableStart(fnolLocationTextField, resultingFileTextField, deviceReportTextField, progressBarText));
 			
-			
+			customiseRows.addActionListener(new ActionListener() 
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					frame.setAlwaysOnTop(false);
+					new Table();
+				}
+			});
 			
 			comboBox.addActionListener(new ActionListener() 
 			{
@@ -372,6 +398,7 @@ public class GuiCode
 					updateProgressBar(pBar, 2100);
 					CollectFnolData.createResultingFile(rfLocation, pBar, progressBarText, panel);
 					updateProgressBar(pBar, 3000);
+					File copyFile = new File(claimsFileCopyPath);
 					CollectFnolData.copyFnolDataIntoSpreadsheet(fnolFiles, deviceReportTextField.getText(), pBar, progressBarText, panel, copyFile);
 					updateProgressBar(pBar, 6200);
 					updateProgressText("Delete Converted Device Report", progressBarText, panel);
@@ -522,4 +549,198 @@ public class GuiCode
 		{
 		}
 	}
-}
+	
+	static class CustomRenderer extends DefaultTableCellRenderer 
+	{
+		private static final long serialVersionUID = 1L;
+
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	    {
+			Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			
+	        if(row == 0 ||row == 8 || row == 27 || row == 55 || row == 76 || row == 86 || row == 93)
+	        {
+	        	cellComponent.setBackground(Color.BLUE);
+	        	cellComponent.setForeground(Color.WHITE);
+	        	cellComponent.setEnabled(false);
+	        } 
+	        else
+	        {
+	        	if(isSelected)
+	        	{
+		        	cellComponent.setBackground(Color.RED);
+		        	cellComponent.setForeground(Color.BLACK);
+		        	cellComponent.setEnabled(true);
+	        	}
+	        	else
+	        	{
+		        	cellComponent.setBackground(Color.WHITE);
+		        	cellComponent.setForeground(Color.BLACK);
+		        	cellComponent.setEnabled(true);
+	        	}
+	        }
+	        /*else if(isSelected)
+	        {
+	        	if(!(row == 0 ||row == 8 || row == 27 || row == 55 || row == 76 || row == 86 || row == 93))
+	        	{
+	        		
+	        		System.out.println(cellComponent.getBackground());
+	        		if(cellComponent.getBackground().getBlue() == 229)
+	        		{
+			        	cellComponent.setBackground(Color.red);
+			        	cellComponent.setEnabled(true);
+	        		}
+	        		else
+	        		{
+	    	        	cellComponent.setBackground(Color.WHITE);
+	    	        	cellComponent.setForeground(Color.BLACK);
+		   	        	cellComponent.setEnabled(true);	
+	        		}
+	        	}
+	        }
+        	else
+        	{
+        		if(cellComponent.getBackground() == Color.RED)
+        		{
+		        	cellComponent.setBackground(Color.red);
+		        	cellComponent.setEnabled(true);
+        		}
+        		else
+        		{
+    	        	cellComponent.setBackground(Color.WHITE);
+    	        	cellComponent.setForeground(Color.BLACK);
+        			cellComponent.setEnabled(true);
+        		}
+        	}
+	        /*else 
+	        {
+	        	if(cellComponent.getBackground() == Color.red && hasFocus)
+	        		return cellComponent;
+	        	else
+	        	{
+		        	cellComponent.setBackground(Color.WHITE);
+		        	cellComponent.setForeground(Color.BLACK);
+		        	cellComponent.setEnabled(true);
+	        	}
+	        }*/
+	        return cellComponent;
+	    }
+	}
+	
+	static class Table
+	{
+		public Table()
+		{
+			JFrame fnolFrame = new JFrame("Select FNOL Rows");
+			Object columnNames[] = {"FNOL Row Title", "Example Data", ""};
+			
+			Object rowData[][] = {{"Tesco DotCom Incident Report Form","",false},{"Date Of Call", "11/01/18",false},{"Start Time Of Call","12:00:00",false},
+					{"Finish Time Of Call","12:30:00",false},{"Has Anyone Sustained Any Injuries","NO",false},{"Have The Emergency Services Attended","NO",false},
+					{"Is The Tesco Vehicle Drivable","YES",false},{"Is Vehicle Recovery Required", "NO",false},{"TESCO DETAILS","",false},{"Tesco Driver's Name","Mr F Name",false},
+					{"Tesco Driver's Date Of Birth","01/01/1990",false},{"Tesco Driver's Employee Number","UK123456789",false},{"Has This Tesco Driver Sustained Injuries","N/A",false},
+					{"If Injured Type Of Injury","N/A",false},{"Tesco Drivers Contact Number","123456789",false},{"Tesco / Agency Driver","Own",false},
+					{"If Agency, Which Agency","N/A",false},{"Store Address","Some Address",false},{"Postcode","MM11MM",false},{"Store Telephone Number","123456789",false},
+					{"Tesco Vehicle Registration Number","DD1X12",false},{"Vehicle Make / Model","IVECO VAN",false},{"Van Number","00001",false},{"Is Vehicle Loaded","YES",false},
+					{"Start Time Of Driver's Shift","18:00",false},{"End Time Of Driver's Shift","22:00",false},{"Tesco Business Division","Tesco Dotcom",false},
+					{"THIRD PARTY DETAILS","",false},{"Driver / Property Occupant Name","S NAME",false},{"Gender Of Third Party","MALE",false},{"Occupation","N/K",false},
+					{"Address","21 Some Address, Somewhere",false},{"Postcode","FF11FF",false},{"Email Address","N/K",false},{"Contact Telephone Number - Home","123456789",false},
+					{"Contact Telephone Number - Mobile","987654321",false},{"Type Of Third Party","Property Owner",false},{"Has This Third Party Sustained Injuries","NO",false},
+					{"If Injured Type Of Injury","N/A",false},{"Was The Third Party Driver In The Vehicle At The Time Of The Incident","N/A",false},
+					{"Third Party Vehicle Registration Number","N/A",false},{"Vehicle Make / Model","N/A",false},{"Number Of People In Third Party Vehicle (Including Driver","1",false},
+					{"Position Of Third Party Vehicle","N/A",false},{"Type of Third Party Property","Wall (Domestic)",false},{"Third party insurer","N/K",false},
+					{"Has BUMP CARD been given to Third Party / Property Owner","YES",false},{"Did the Third Party admit Liability at the scene of the collision","NO",false},
+					{"Description of damage to Third Party vehicle / Property","ENDS BRICKS LOOSE",false},{"Was there any pre-existing damage to the Third Party Vehicle","NO",false},
+					{"Vehicle / Property owner name (If different to Driver / Property occupant name)","N/A",false},{"Owner Contact Number","N/A",false},{"Owner Email Address","N/A",false},
+					{"Owner Address","N/A",false},{"Owner Postcode","N/A",false},{"COLLISION CIRCUMSTANCES","",false},{"Collision time","21:00",false},{"Collision date","01/01/18",false},
+					{"Collision location","SOME ADDRESS, ADDRESS",false},{"Collision sub location","On The Road",false},{"Collision causation code","HIT TP WALL/BUILDING/GATE/OBJECT",false},
+					{"Type of road","Single carriageway C road",false},{"Position Of Tesco Vehicle on road","TRAVELLING FORWARDS",false},{"Road Conditions","Wet",false},
+					{"Weather Conditions","Dry Overcast",false},{"Description of damage to Tesco vehicle","NEARSID SIDE STEP BENT.",false},
+					{"Was another Third Party involved causing damage to the Tesco vehicle","YES",false},{"If No Cause of damage ","N/A",false},
+					{"Did the collision happen in daylight","NO",false},{"Did Tesco driver admit Liability at the scene of the collision","NO",false},
+					{"Have you completed a sketch of the collision scene","NO",false},{"Please give a brief description of the collision circumstances ","Some description",false},
+					{"In Tesco’s Opinion do you believe the third party property / vehicle is owned by Tesco.","NO",false},{"Have photographs been taken","YES",false},
+					{"Was driver aware incident occurred","YES",false},{"Was the driver in the area at the time of incident","YES",false},{"WITNESS DETAILS","",false},
+					{"Were there any witnesses","NO",false},{"Witness 1 name","N/A",false},{"Witness 1 address","N/A",false},{"Witness 1 postcode","N/A",false},
+					{"Witness 1 telephone number","N/A",false},{"Witness 2 name","N/A",false},{"Witness 2 address","N/A",false},{"Witness 2 postcode","N/A",false},
+					{"Witness 2 telephone number","N/A",false},{"POLICE DETAILS","",false},{"Did the police attend the scene","NO",false},{"Police Officer name ","N/A",false},
+					{"Police Officer badge number","N/A",false},{"Police station","N/A",false},{"Police station telephone number","N/A",false},{"Police Reference","N/A",false},
+					{"FURTHER INFORMATION","",false},{"sopp+sopp further comments","NONE",false},{"sopp+sopp Reference number","000000",false},{"Plexus Reference Number","N/A",false},
+					{"Who took original call","sopp+sopp",false},{"Full Name and Position of person reporting incident","SOME NAME - CDD",false},{"Driver Trainer Name","N/A",false},
+					{"Driver Trainer Email Address","N/A",false}};
+			
+			
+			JPanel panel = new JPanel();
+			JPanel buttonPanel = new JPanel();
+			panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+			buttonPanel.setLayout(new FlowLayout());
+			
+			
+			final JTable rowTable = new JTable(rowData, columnNames);
+			
+			DefaultTableModel model = new DefaultTableModel(rowData, columnNames)
+			{
+				private static final long serialVersionUID = 1L;
+
+				@Override
+	            public Class<?> getColumnClass(int column) 
+		        {
+	                switch (column) 
+	                {
+	                    case 0:
+	                        return String.class;
+	                    case 1:
+	                        return String.class;
+	                    case 2:
+	                    {
+	                    	rowTable.getColumnModel().getColumn(2).setPreferredWidth(10);
+	                    	return Boolean.class;
+	                    }
+	                        
+	                }
+					return null;
+	            }
+			};
+			
+			
+			rowTable.setModel(model);
+			rowTable.setDefaultEditor(Object.class, null);
+			JButton exit = new JButton("Exit");
+			JButton save = new JButton("Save");
+			JScrollPane scrollPane = new JScrollPane(rowTable);
+			
+			rowTable.setRowSelectionInterval(0, 0);
+			rowTable.getSelectionModel().addSelectionInterval(0, 0);
+			rowTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+			rowTable.getColumnModel().getColumn(0).setCellRenderer(new CustomRenderer());
+			rowTable.getColumnModel().getColumn(1).setCellRenderer(new CustomRenderer());
+			//rowTable.getColumnModel().getColumn(2).setCellRenderer(new CustomRenderer());
+			
+			rowTable.getTableHeader().setReorderingAllowed(false);
+			
+			rowTable.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+			
+			panel.add(scrollPane, BorderLayout.CENTER);
+			buttonPanel.add(exit);
+			buttonPanel.add(save);
+			//panel.add(buttonPanel);
+			
+			fnolFrame.add(panel);
+			//fnolFrame.add(scrollPane, BorderLayout.CENTER);
+			fnolFrame.setSize(800, 900);
+			fnolFrame.setVisible(true);
+			fnolFrame.setLocationRelativeTo(null);
+			fnolFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			rowTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() 
+			{
+				public void valueChanged(ListSelectionEvent e)
+				{
+					if(!(e.getValueIsAdjusting()))
+					{
+						System.out.println(rowTable.getValueAt(rowTable.getSelectedRow(),0).toString());
+					}
+				}
+			});
+		}
+	}
+}	
